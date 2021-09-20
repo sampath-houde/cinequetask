426d20d5e902e9b3412e0e9091b54ba6566dc90d
Sampath <shoude42@gmail.com>
2021-09-21 01:35:39 +0530

    added back func in register screen

--- app/src/main/java/com/task/cinqueuex/auth/ui/fragment/RegisterFragment.kt
+++ app/src/main/java/com/task/cinqueuex/auth/ui/fragment/RegisterFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.task.cinqueuex.R
+import com.task.cinqueuex.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+
+
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.bind(view)
+
+        binding.backBtn.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        return view
+
     }
 
 }
